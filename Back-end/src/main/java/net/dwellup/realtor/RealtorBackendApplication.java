713b0bcbccb19a7d99bcc298a5510aa9c5b53141
AR Mahee <22235103478@cse.bubt.edu.bd>
2026-06-08 22:31:13 +0600

    springboot backend files

---
+++ Back-end/src/main/java/net/dwellup/realtor/RealtorBackendApplication.java
@@ -0,0 +1,13 @@
+package net.dwellup.realtor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtorBackendApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/net/dwellup/realtor/RealtorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dwellup.realtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
